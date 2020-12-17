85787e565892bff7c5919c95f6d50fa19a14e10f
Puchunim <pedro.spezzialemotta.rodrigues@gmail.com>
2020-12-17 02:39:17 -0300

    Exercícios do 011 ao 020

---
+++ 004 - Variáveis e tipos de dados/006 - EXERCÍCIOS/011-020/main.py
@@ -0,0 +1,68 @@
+# 11 - Leia uma velocidade em m/s e aprente-a convertida em km/h (K = M * 3.6)
+print('# EX 011')
+ms = float(input('Digite um valor em M/s: '))
+print(f'O valor {ms}m/s, para Km/h, equivale a {ms * 3.6}')
+print('===+===\n\n')
+
+
+# 12 - Leia uma distância em milhas e aprente-a convertida em quilômetros (K = 1.61 * M)
+print('# EX 012')
+mi = float(input('Digite um valor em milhas: '))
+print(f'O valor de {mi} milhas, para quilômetros, equivale a {1.61 * mi}')
+print('===+===\n\n')
+
+
+# 13 - Leia uma distância em quilômetros e apresente-a convertida em milhas (M = K/1.61)
+print('# EX 013')
+qui = float(input('Digite um valor em quilômetros: '))
+print(f'{qui} quilômetros equivalem a {qui / 1.61} milhas')
+print('===+===\n\n')
+
+
+# 14 - Leia um ângulo em graus e apresente-o convertido em radianos (R = G * pi/180)
+print('# EX 015')
+ang = float(input('Digite um valor em ângulos: '))
+print(f'{ang} graus (ângulos) equivalem a {ang * (3.14/180)} radianos')
+print('===+===\n\n')
+
+
+# 15 - Leia um ângulo em radianos e apresente-o convertido em graus (G = R * 180/pi)
+print('# EX 015')
+rad = float(input('Digite um valor em radianos: '))
+print(f'{rad}rad equivalem a {rad * (180/3.14)}°')
+print('===+===\n\n')
+
+
+# 16 - Leia um valor de comprimento em polegadas e apresente-o convertido em centímetros (C = P * 2.54)
+print('# EX 016')
+inch = float(input('Digite um comprimento em polegadas: '))
+print(f'{inch} polegadas equivalem a {inch * 2.54} centímetros')
+print('===+===\n\n')
+
+
+# 17 - Leia um valor de comprimento em centímetros e apresente-o convertido em polegadas (P = C/2.54)
+print('# EX 017')
+cm = float(input('Digite um valor em centímetros: '))
+print(f'{cm} centímetros equivalem a {cm / 2.54} polegadas')
+print('===+===\n\n')
+
+
+# 18 - Leia um valor de volume em metros cúbicos e aprente-o convertido em litros (L = 1000 * M)
+print('# EX 018')
+m3 = float(input('Digite um valor em metros cúbicos: '))
+print(f'{m3}m³ equivalem a {m3 * 1000}l')
+print('===+===\n\n')
+
+
+# 19 - Leia um valor de volume em litros e apresente-o convertido em metros cúbicos (M = L/1000)
+print('# EX 019')
+li = float(input('Digite um valor em litros'))
+print(f'{li}l equivalem a {li / 1000}m³')
+print('===+===\n\n')
+
+
+# 20 - Leia um valor de massa em quilogramas e apresente-o convertido em libras (L = K/0.45)
+print('# 020')
+qui = int(input('Digite um valor em quilogramas: '))
+print(f'{qui}kg equivalem a {qui / 0.45} libras')
+print('===+===')
